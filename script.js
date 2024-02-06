5d1a1956ffe4dbc1b78fb33b123c207dba1174a5
Richard4375 <140004673+Richard4375@users.noreply.github.com>
2024-02-06 22:44:08 +0000

    completed tasks

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // 1. Convert to an arrow function.
-function greet() {
-    return "Hello, world!";
-}
+// function greet() {
+//     return "Hello, world!";
+// }
   
-
+const greet = () => {
+  return "Hello, World"
+} 
 // 2. Convert to an arrow function.
-function double(number) {
-    return number * 2;
-}
+// function double(number) {
+//     return number * 2;
+// }
   
-
+const double = (number) => {
+  return number * 2 
+}
 // 3. Convert to an arrow function.
-function multiply(a, b) {
-    return a * b;
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+const multiply = (a, b) => {
+  return a * b
 }
-  
-  
 
 console.log(greet())  // should print "Hello, world!"
 console.log(double(3)) // should print 6
